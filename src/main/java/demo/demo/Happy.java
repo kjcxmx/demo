b07efcc9588b742b8a25b80db2bce2d7957247d6
zf <kjcxmx>
2019-01-03 00:05:53 +0800

    branch hot-fix

--- src/main/java/demo/demo/Happy.java
+++ src/main/java/demo/demo/Happy.java
@@ -2,7 +2,8 @@ package demo.demo;
 
 public class Happy {
   public static void main(String[] args) {
-    System.out.println("Hello");
+	System.out.println("Hello");
 	System.out.println("Hello nihao");
+	System.out.println("修复bug");
 }
 }
